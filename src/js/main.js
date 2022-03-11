4f07066fc421f846bc3c4f401ae788ad66e09a29
David T <matkatdavid@gmail.com>
2022-03-10 18:09:00 -0700

    Added dynamic products & filter

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import ProductData from "./productData.js";
+import ProductListing from "./productList.js";
+
+const dataSource = new ProductData("tents");
+const productList = document.querySelector(".product-list");
+const myList = new ProductListing("tents", productList, dataSource);
+myList.init();
